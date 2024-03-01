39886640f71f00248b90e07e8ac7dfacb3119d71
Zachary Swensrude <swensrudez@gmail.com>
2024-03-01 14:33:44 -0700

    setup backend for use

---
+++ backend/database/database.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+mongoose.set('strictQuery', true);
+
+const connectDB = async () => {
+  // keeping env comment for later when we have to deploy
+  const url = 'mongodb://localhost:27017/315-final'; //process.env.MONGO_URI || 'mongodb://localhost:27017/courses';
+  try {
+    const connection = await mongoose.connect(url, {
+      useUnifiedTopology: true,
+    });
+  } catch (e) {
+    console.log("Failed to connect to courses database: ", e);
+  }
+  return url;
+};
+
+export default connectDB;
---
+++ backend/index.js
@@ -0,0 +1,37 @@
+// Code adapted from https://github.com/mrchenliang/learning-node
+import express from "express";
+import connectDB from './database/database.js';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+// dotenv is for when we want to use environment variables (for deploying)
+//import dotenv from 'dotenv';
+//dotenv.config();
+
+const app = express();
+app.use(cors());
+const port = 8080; //process.env.PORT || 8080;
+
+const url = await connectDB();
+
+// allows app to deal with url encoded and json requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// set to use the courses router if at /courses
+//app.use('/courses', courseRouter);
+
+// Not found
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!");
+});
+  
+// Error
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(port, function () {
+  console.log(`🚀 Fire app listening on port ${port}, connected to ${url}!`);
+});
+  
