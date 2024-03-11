4ba8e689578aa8fb001be1fe22429b77ba407c64
Ife Sunmola <sunmolaife@gmail.com>
2024-03-10 19:54:59 -0600

    Changed 'throw Error' in catch blocks to actually show the error

--- backend/repositories/manager.repository.js
+++ backend/repositories/manager.repository.js
@@ -1,86 +1,86 @@
 // Code adapted from https://github.com/mrchenliang/learning-node
 
 import Manager from "../models/manager.model.js";
-import { validateRestaurant } from "./restaurant.repository.js";
+import {validateRestaurant} from "./restaurant.repository.js";
 
 export const getManagersFromRepository = async (query) => {
-  try {
-    const managers = await Manager.find(query).populate("restaurantId");
-    return managers;
-  } catch (e) {
-    throw Error("Error while fetching managers: ", e);
-  }
+    try {
+        const managers = await Manager.find(query).populate("restaurantId");
+        return managers;
+    } catch (e) {
+        throw Error(`Error while fetching managers: ${e}`);
+    }
 }
 
 export const updateManagersInRepository = async (managerID, query) => {
-  let exists = await managerExists(managerID);
-  if (!exists) {
-    return -1;
-  }
-  try {
-    const manager = await Manager.findOneAndUpdate(
-      { id: managerID },
-      { $set: query.body },
-      { new: true }
-    ).lean();
-    return manager;
-  } catch (e) {
-    throw Error("Error while updating manager: ", e);
-  } 
+    let exists = await managerExists(managerID);
+    if (!exists) {
+        return -1;
+    }
+    try {
+        const manager = await Manager.findOneAndUpdate(
+          {id: managerID},
+          {$set: query.body},
+          {new: true}
+        ).lean();
+        return manager;
+    } catch (e) {
+        throw Error(`Error while updating manager: ${e}`);
+    }
 }
 
 export const deleteManagerFromRepository = async (managerID) => {
-  try {
-    const manager = await Manager.findOneAndDelete({ id: managerID });
-    return manager;
-  } catch (e) {
-    throw Error("Error while deleting a manager: ", e);
-  }
+    try {
+        const manager = await Manager.findOneAndDelete({id: managerID});
+        return manager;
+    } catch (e) {
+        throw Error(`Error while deleting a manager: ${e}`);
+    }
 }
 
-export const createManagerInRepository = async ({ name, email, address, restaurantId }) => {
-  try {
-    // input validation
-    const restaurantObjectId = await validateRestaurant(restaurantId);
+export const createManagerInRepository = async ({name, email, address, restaurantId}) => {
+    try {
+        // input validation
+        const restaurantObjectId = await validateRestaurant(restaurantId);
 
-    // Generate a unique ID for the new manager
-    const newManagerId = await getUniqueManagerID();
+        // Generate a unique ID for the new manager
+        const newManagerId = await getUniqueManagerID();
 
-    const newManager = new Manager({
-      id: newManagerId,
-      name,
-      email,
-      address,
-      restaurantId: restaurantObjectId
-    });
+        const newManager = new Manager({
+            id: newManagerId,
+            name,
+            email,
+            address,
+            restaurantId: restaurantObjectId
+        });
 
-    // Save the new manager
-    const savedManager = await newManager.save();
-    return savedManager;
-  } catch (error) {
-    throw new Error("Error creating manager: " + error.message);
-  }
+        // Save the new manager
+        const savedManager = await newManager.save();
+        return savedManager;
+    } catch (error) {
+        throw new Error("Error creating manager: " + error.message);
+    }
 }
 
 
 // ------------- Helper functions ------------- //
-  
+
 // This func gets the highest id in the database and increments it by one so its always a unique id
 const getUniqueManagerID = async () => {
-  const maxIdDocument = await Manager.findOne({}, { id: 1 }).sort({ id: -1 });
-  const maxId = maxIdDocument ? maxIdDocument.id : 0;
-  return maxId + 1;
+    const maxIdDocument = await Manager.findOne({}, {id: 1}).sort({id: -1});
+    const maxId = maxIdDocument ? maxIdDocument.id : 0;
+    return maxId + 1;
 }
 
 // Function to check if a manager exists
 const managerExists = async (managerID) => {
-  const manager = await Manager.findOne({ id: managerID });
-  return manager ? true : false;
+    const manager = await Manager.findOne({id: managerID});
+    return manager ? true : false;
 }
 
 // Function to validate a manager
 export const validateManager = async (pmanagerId) => {
-  const manager = await Manager.findOne({ id: pmanagerId });
-  if (!manager) throw Error("Manager does not exist");
-  return manager._id;
+    const manager = await Manager.findOne({id: pmanagerId});
+    if (!manager) throw Error("Manager does not exist");
+    return manager._id;
 }
--- backend/repositories/restaurant.repository.js
+++ backend/repositories/restaurant.repository.js
@@ -3,55 +3,55 @@
 import Restaurant from "../models/restaurant.model.js";
 
 export const getRestaurantsFromRepository = async (query) => {
-  try {
-    const restaurants = await Restaurant.find(query);
-    return restaurants;
-  } catch (e) {
-    throw Error("Error while fetching restaurants: ", e);
-  }
+    try {
+        const restaurants = await Restaurant.find(query);
+        return restaurants;
+    } catch (e) {
+        throw Error(`Error while fetching restaurants: ${e}`);
+    }
 }
 
 export const updateRestaurantsInRepository = async (restaurantID, query) => {
-  let exists = await restaurantExists(restaurantID);
-  if (!exists) {
-    return -1;
-  }
-  try {
-    const restaurant = await Restaurant.findOneAndUpdate(
-      { id: restaurantID },
-      { $set: query.body },
-      { new: true }
-    ).lean();
-    return restaurant;
-  } catch (e) {
-    throw Error("Error while updating restaurant: ", e);
-  } 
+    let exists = await restaurantExists(restaurantID);
+    if (!exists) {
+        return -1;
+    }
+    try {
+        const restaurant = await Restaurant.findOneAndUpdate(
+          {id: restaurantID},
+          {$set: query.body},
+          {new: true}
+        ).lean();
+        return restaurant;
+    } catch (e) {
+        throw Error(`Error while updating restaurant: ${e}`);
+    }
 }
 
 export const deleteRestaurantFromRepository = async (restaurantID) => {
-  try {
-    const restaurant = await Restaurant.findOneAndDelete({ id: restaurantID });
-    return restaurant;
-  } catch (e) {
-    throw Error("Error while deleting a restaurant: ", e);
-  }
+    try {
+        const restaurant = await Restaurant.findOneAndDelete({id: restaurantID});
+        return restaurant;
+    } catch (e) {
+        throw Error(`Error while deleting a restaurant: ${e}`);
+    }
 }
 
 
 export const createRestaurantInRepository = async (payload) => {
-  try {
-    // get a new id
-    const newId = await getUniqueRestaurantID();
-    console.log("newId", newId)
-    // add it to the payload obj
-    payload = {...payload, id: newId};
-    // then add to db
-    const newRestaurant = new Restaurant(payload);
-    const savedRestaurant = await newRestaurant.save();
-    return savedRestaurant;
-  } catch (e) {
-    throw Error("Error while creating a restaurant: ", e);
-  }
+    try {
+        // get a new id
+        const newId = await getUniqueRestaurantID();
+        console.log("newId", newId)
+        // add it to the payload obj
+        payload = {...payload, id: newId};
+        // then add to db
+        const newRestaurant = new Restaurant(payload);
+        const savedRestaurant = await newRestaurant.save();
+        return savedRestaurant;
+    } catch (e) {
+        throw Error(`Error while creating a restaurant: ${e}`);
+    }
 }
 
 
@@ -59,20 +59,20 @@ export const createRestaurantInRepository = async (payload) => {
 
 // This func gets the highest id in the database and increments it by one so its always a unique id
 const getUniqueRestaurantID = async () => {
-  const maxIdDocument = await Restaurant.findOne({}, { id: 1 }).sort({ id: -1 });
-  const maxId = maxIdDocument ? maxIdDocument.id : 0;
-  return maxId + 1;
+    const maxIdDocument = await Restaurant.findOne({}, {id: 1}).sort({id: -1});
+    const maxId = maxIdDocument ? maxIdDocument.id : 0;
+    return maxId + 1;
 }
 
 // Function to check if a restaurant exists
 const restaurantExists = async (restaurantID) => {
-  const restaurant = await Restaurant.findOne({ id: restaurantID });
-  return restaurant ? true : false;
+    const restaurant = await Restaurant.findOne({id: restaurantID});
+    return restaurant ? true : false;
 }
 
 // Function to validate a restaurant
 export const validateRestaurant = async (restaurantId) => {
-    const restaurant = await Restaurant.findOne({ id: restaurantId });
+    const restaurant = await Restaurant.findOne({id: restaurantId});
     if (!restaurant) throw new Error("Invalid Restaurant");
     return restaurant._id;
 }
