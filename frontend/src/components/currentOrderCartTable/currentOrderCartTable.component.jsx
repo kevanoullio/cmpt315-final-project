11b9f41b1b29a6759e4662aaa2a51fcaba549311
Kevin Ulliac <kevanoullio@outlook.com>
2024-03-14 16:27:21 -0600

    Added .editorconfig file, updated .gitignore file.

--- frontend/src/components/currentOrderCartTable/currentOrderCartTable.component.jsx
+++ frontend/src/components/currentOrderCartTable/currentOrderCartTable.component.jsx
@@ -12,40 +12,40 @@ import "./currentOrderCartTable.styles.css";
  * @returns {JSX.Element} - The menuItem table component
  */
 const CurrentOrderCartTable = ({ menuItemsInCart, onRemoveFromCart, onCheckout }) => {
-    // Render the menuItem table
-    return (
-        <div className="current-order-cart-table">
-            {/* <h2 className="h2">Your Order</h2> */}
-            <BootstrapTable className="bootstrap-table" striped bordered hover>
-                <thead className="custom-header">
-                    <tr>
-                        <th>Menu Item</th>
-                        <th>Price</th>
-                        {/* <th>#</th> >>> can add later if decide to have items stack up */}
-                        <th>Cart</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {menuItemsInCart && menuItemsInCart.map((menuItem, index) => (
-                        <MenuItem
-                            key={index} 
-                            menuItem={menuItem}
-                            onRemoveFromCart={onRemoveFromCart} 
-                        />
-                    ))}
-                </tbody>
-            </BootstrapTable>
-            <div className="subtotal-checkout-container">
-                <h3>Subtotal: ${menuItemsInCart.reduce((total, menuItem) => total + menuItem.price, 0).toFixed(2)}</h3>
-                <Button variant="success" size="lg" onClick={() => onCheckout(menuItemsInCart)}>
-                    Checkout
-                </Button>
-                <Button className="previous-orders-button" variant="secondary" size="md" block>
-                    Previous Orders
-                </Button>
-            </div>
-        </div>
-    );
+  // Render the menuItem table
+  return (
+    <div className="current-order-cart-table">
+      {/* <h2 className="h2">Your Order</h2> */}
+      <BootstrapTable className="bootstrap-table" striped bordered hover>
+        <thead className="custom-header">
+          <tr>
+            <th>Menu Item</th>
+            <th>Price</th>
+            {/* <th>#</th> >>> can add later if decide to have items stack up */}
+            <th>Cart</th>
+          </tr>
+        </thead>
+        <tbody>
+          {menuItemsInCart && menuItemsInCart.map((menuItem, index) => (
+            <MenuItem
+              key={index}
+              menuItem={menuItem}
+              onRemoveFromCart={onRemoveFromCart}
+            />
+          ))}
+        </tbody>
+      </BootstrapTable>
+      <div className="subtotal-checkout-container">
+        <h3>Subtotal: ${menuItemsInCart.reduce((total, menuItem) => total + menuItem.price, 0).toFixed(2)}</h3>
+        <Button variant="success" size="lg" onClick={() => onCheckout(menuItemsInCart)}>
+          Checkout
+        </Button>
+        <Button className="previous-orders-button" variant="secondary" size="md" block>
+          Previous Orders
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default CurrentOrderCartTable;
