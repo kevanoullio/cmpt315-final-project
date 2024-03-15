58e74328043497e208ba8a285ff29a13714611fa
Zachary Swensrude <swensrudez@gmail.com>
2024-03-15 17:54:35 -0600

    auto formatting

--- frontend/src/components/restaurantCard/restaurantCard.component.jsx
+++ frontend/src/components/restaurantCard/restaurantCard.component.jsx
@@ -10,20 +10,20 @@ import './restaurantCard.styles.css';
  */
 
 const RestaurantCard = ({ restaurant, onClick }) => (
-	<Card className="card"  onClick={onClick}>
-		{/* <Card.Img variant="top" src={restaurant.image} /> */}
-		<Card.Img variant="top"
-			src="https://img.freepik.com/free-vector/vector-pastel-plate-food-sticker-clipart_53876-170585.jpg?w=826&t=st=1710138142~exp=1710138742~hmac=b4c26387026e4075a450797389cbdbb80a8c9a7ee760731925f7ac359d00134e" />
-		<Card.Body>
-			<Card.Title>{restaurant.name}</Card.Title>
-			<Card.Text>
-				{/* {restaurant.description} */}
-				{restaurant.address}
-				Restaurant description here
-			</Card.Text>
-			{/* <Button variant="primary">Go somewhere</Button> */}
-		</Card.Body>
-	</Card>
+  <Card className="card" onClick={onClick}>
+    {/* <Card.Img variant="top" src={restaurant.image} /> */}
+    <Card.Img variant="top"
+      src="https://img.freepik.com/free-vector/vector-pastel-plate-food-sticker-clipart_53876-170585.jpg?w=826&t=st=1710138142~exp=1710138742~hmac=b4c26387026e4075a450797389cbdbb80a8c9a7ee760731925f7ac359d00134e" />
+    <Card.Body>
+      <Card.Title>{restaurant.name}</Card.Title>
+      <Card.Text>
+        {/* {restaurant.description} */}
+        {restaurant.address}
+        Restaurant description here
+      </Card.Text>
+      {/* <Button variant="primary">Go somewhere</Button> */}
+    </Card.Body>
+  </Card>
 );
 
 export default RestaurantCard;
